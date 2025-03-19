3175def626e5c7f37bd0441cf0791b3354bd49c9
Youssef97dev <youssefmoussafi97@gmail.com>
2025-03-19 09:37:47 +0000

    add animate writing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Link from "next/link";
 import { useTranslation } from "react-i18next";
+import { TypeAnimation } from "react-type-animation";
 
 const About = () => {
   const { t } = useTranslation();
@@ -11,7 +12,12 @@ const About = () => {
       className="w-full flex flex-col justify-start lg:justify-center items-center gap-6 h-full text-center px-3 xl:px-60 2xl:px-96 py-24 2xl:py-36"
     >
       <h1 className="lg:text-[26px] text-[19px] leading-[36px]  font-sackerCothic uppercase text-primary">
-        {t("about.title")}
+        <TypeAnimation
+          sequence={[t("about.title"), 2000, t("about.title"), 2000]}
+          wrapper="span"
+          speed={10}
+          repeat={Infinity}
+        />
       </h1>
       <p className="text-[16px] leading-[34px] px-0 2xl:px-12 ">
         {t("about.content_1")}
